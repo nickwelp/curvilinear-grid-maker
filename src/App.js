6a324d3f75364083fb92658185c359d23e11d97d
nickwelp <nickwelp@gmail.com>
2022-09-22 11:56:04 -0500

    spelling fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div style={{maxWidth: "600px", margin: 'auto', textAlign: 'left', fontSize: '12px'}}>
         <p>This app is to help make 6 point perpesctive grids, you can save to your device.</p>
         <p>Set the width and height of the Clipping Rectangle to represent paper you'd print this grid upon. 400DPI for 11x17 is 4400x6800.</p>
-        <p>Then you can manipulate the size of the primary shperoid. If you set the width and height of the Primary Spheroid to the same as the Clipping Rectangle, the boundries will be at the edge.</p>
+        <p>Then you can manipulate the size of the primary spheroid. If you set the width and height of the Primary Spheroid to the same as the Clipping Rectangle, the boundries will be at the edge.</p>
         <p>You can tilt the primary shperoid, and add upper extensions to it. The reflection spheroids do not have upper extension manipulation.</p>
         <p>When you're done, click 'save image' to download a PNG of the defined height and width.</p>
       </div>
